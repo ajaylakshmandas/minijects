b25e862525b12154ad6317c947c5ae838e4f80c0
ajaylakshmandas <67698346+ajaylakshmandas@users.noreply.github.com>
2021-05-31 23:07:55 +0530

    Update madlib.py

--- madlib.py
+++ madlib.py
@@ -7,6 +7,4 @@ besttime=input("enter your best time you have spent with ajay:")
 friendsname=input("enter your best friends name:")
 thosedays=input("enter y0ur golden day&memories:")
 madlib=f"{Name}you are such a important person in my life.I love u to the core my best friend {Name} . we both have spet a beautiful time do you remember {besttime}.i knew you are beautiful person in the world {Name} be brave{Name}.i know we are the best frients forever {friendsname}.we still remis forever {thosedays}were golden days love u by ajay"
-
-
 print(madlib)
